ee0e219d2d5065e9c9b430b0585bc5bc9ebb3ba7
Valer-Muresan <muresanvaler86@yahoo.com>
2018-06-20 18:47:01 +0300

    Update Html, CSS, JS, README

--- README.md
+++ README.md
@@ -3,12 +3,33 @@
 ## Table of Contents
 
 * [Instructions](#instructions)
-* [Contributing](#contributing)
+* [How_The_Game_Works](#how the game works)
+* [Dependencies](#dependencies)
+* [Test](#test)
+
+
 
 ## Instructions
 
+The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
 
-## Contributing
+## How The Game Works
 
+The game board consists of sixteen "cards" arranged in a grid. The deck is made up of eight different pairs of cards, each with different symbols on one side. The cards are arranged randomly on the grid with the symbol faced down.
 
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+Each turn:
+
+* The player flips one card over to reveal its underlying symbol.
+* The player then turns over a second card, trying to find the corresponding card with the same symbol.
+* If the cards match, both cards stay flipped over.
+* If the cards do not match, both cards are flipped face down.
+* The game ends once all cards have been correctly matched.
+
+## Dependencies
+* [Udacity Fend](https://github.com/udacity/fend-project-memory-game)<br />
+* [Udacity Study Jam](https://www.youtube.com/watch?v=G8J13lmApkQ)<br />
+* [Udacity Webinar](https://www.youtube.com/watch?v=oECVwum-7Zc)
+
+## Test
+* Memory Game [Play]()
--- js/app.js
+++ js/app.js
@@ -1,7 +1,201 @@
-/*
- * Create a list that holds all of your cards
- */
 
+//  Built the list with cards
+
+const pics = [
+  "fa fa-diamond",
+  "fa fa-diamond",
+  "fa fa-paper-plane-o",
+  "fa fa-paper-plane-o",
+  "fa fa-anchor",
+  "fa fa-anchor",
+  "fa fa-bolt",
+  "fa fa-bolt",
+  "fa fa-cube",
+  "fa fa-cube",
+  "fa fa-leaf",
+  "fa fa-leaf",
+  "fa fa-bicycle",
+  "fa fa-bicycle",
+  "fa fa-bomb",
+  "fa fa-bomb"
+];
+const cardsContainer = document.querySelector(".deck");
+let openedCards = [];
+let matchedCards = [];
+let firstClick = true;
+const modal = document.getElementById("myModal");
+const starsContainer = document.querySelector(".stars");
+const star = `<li><i class="fa fa-star"></i></li>`;
+const movesContainer = document.querySelector(".moves");
+let moves = 0;
+const restartBtn = document.querySelector(".restart");
+const timerContainer = document.querySelector(".timer");
+let liveTimer,
+    totalSeconds = 0;
+
+ // Start the game function
+function startGame() {
+  const shuffledPics = shuffle(pics);
+    for(let i = 0; i < pics.length; i++) {
+        const card = document.createElement("li");
+        card.classList.add("card");
+        card.innerHTML = `<i class="${pics[i]}"></i>`;
+        cardsContainer.appendChild(card);
+        // Call the click function
+        click(card);
+    }
+}
+
+// Create the timer
+timerContainer.innerHTML = totalSeconds + 's';
+ function startTimer() {
+    liveTimer = setInterval(function() {
+        totalSeconds++;
+        timerContainer.innerHTML = totalSeconds + 's';
+    }, 1000);
+}
+function stopTimer() {
+    clearInterval(liveTimer);
+}
+
+// Create Reset function
+function reset() {
+    matchedCards = [];
+    moves = 0;
+    movesContainer.innerHTML = moves;
+    starsContainer.innerHTML = star + star + star;
+    stopTimer();
+    firstClick = true;
+    totalSeconds = 0;
+    timerContainer.innerHTML = totalSeconds + "s";
+}
+
+// Create restart
+restartBtn.addEventListener("click", function() {
+    cardsContainer.innerHTML = "";
+    //Call Start game function
+    startGame();
+    // Game reset
+    reset();
+});
+
+// Create Modal function from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal
+function showModal() {
+modal.style.display = "block";
+// modal close element
+const span = document.getElementsByClassName("close")[0];
+//play again element
+const btn = document.getElementById("myBtn");
+//quit element
+const qbtn = document.getElementById("qBtn");
+// When the user clicks on <button> (Play Again!), restart the game
+btn.addEventListener ('click', function() {
+    modal.style.display = "none";
+    cardsContainer.innerHTML = "";
+    startGame();
+    reset();
+});
+// When the user clicks on <button> (Quit!), reload the page
+qbtn.onclick = function() {
+  modal.style.display ="none";
+  location.reload();
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+// Set the modal's moves
+const movesInModal = document.querySelector("#allMoves");
+movesInModal.innerHTML = ` ${moves + 1}`;
+// Set the modal's time
+const secondsInModal = document.querySelector("#timeInSeconds");
+secondsInModal.innerHTML = totalSeconds;
+// Set the modal's Rating
+const starsInModal = starsContainer.innerHTML;
+document.getElementById("totalStars").innerHTML = starsInModal;
+}
+
+// Game over function
+function gameOver() {
+    if(matchedCards.length === pics.length) {
+        // Call the Stop the time function
+        stopTimer();
+        // Call the modal function
+        showModal();
+    }
+}
+
+// Create the rating
+starsContainer.innerHTML = star + star + star;
+function rating() {
+    if( moves < 17) {
+        starsContainer.innerHTML = star + star + star;
+    } else if( moves < 25) {
+        starsContainer.innerHTML = star + star;
+    } else {
+        starsContainer.innerHTML = star;
+    }
+}
+
+// Create the move
+movesContainer.innerHTML = 0;
+function addMove() {
+    moves++;
+    movesContainer.innerHTML = moves;
+    // Call for Rating function
+    rating();
+}
+
+// Compare the 2 cards function
+function compare(secondCard, firstCard) {
+    // Try to match the cards
+    if(secondCard.innerHTML === firstCard.innerHTML) {
+        // Cards are matched
+        secondCard.classList.add("match");
+        firstCard.classList.add("match");
+        matchedCards.push(secondCard, firstCard);
+        openedCards = [];
+        // Call for Game over function
+        gameOver();
+    } else {
+      // Set the time for matching the cards, in this case 300ms
+        setTimeout(function() {
+            secondCard.classList.remove("open", "show", "disable");
+            firstCard.classList.remove("open", "show", "disable");
+        }, 300);
+        openedCards = [];
+    }
+    // Call for Add move function
+    addMove();
+}
+
+// Create the Click Event
+// Click Function
+function click(card) {
+    // Create Click Event for card
+    card.addEventListener("click", function() {
+        if(firstClick) {
+            // Call Start the timer function
+            startTimer();
+            // Change the First Click value
+            firstClick = false;
+        }
+        const secondCard = this;
+        const firstCard = openedCards[0];
+        // Opened card
+        if(openedCards.length === 1) {
+            card.classList.add("open", "show", "disable");
+            openedCards.push(this);
+            // Call the Compare function
+            compare(secondCard, firstCard);
+        } else {
+            secondCard.classList.add("open", "show", "disable");
+            openedCards.push(this);
+        }
+    });
+}
+// Call Start the game function
+startGame();
 
 /*
  * Display the cards on the page
@@ -24,7 +218,6 @@ function shuffle(array) {
 
     return array;
 }
-
 
 /*
  * set up the event listener for a card. If a card is clicked:
